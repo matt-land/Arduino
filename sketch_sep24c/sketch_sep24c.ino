d7fd25a202cc734b330b20db01d750ff08bc040e
Matt Land <mland@saleamp.com>
2013-11-25 23:24:03 -0600

    random display increments

--- sketch_sep24c/sketch_sep24c.ino
+++ sketch_sep24c/sketch_sep24c.ino
@@ -1,43 +1,52 @@
 #include <LiquidCrystal.h>
 #include <stdlib.h>
-int RS = 8;
-int RW = 11;
-int Enable = 9;
+const int width = 16;
+const int height = 2;
+const int RS = 8;
+const int RW = 11;
+const int Enable = 9;
 LiquidCrystal lcd(RS, RW, Enable, 4, 5, 6, 7);
 
 
-int numDigits(int number)
-{
-    int digits = 0;
-    if (number < 0) digits = 1; // remove this line if '-' counts as a digit
-    while (number) {
-        number /= 10;
-        digits++;
-    }
-    return digits;
-}
+//setup the LCD display
+int myLcd [width][height];
 
 
 void setup() {
   // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  //lcd.print("hello, world!");
+  lcd.begin(width, height);
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      //assign a random value to each display position
+      myLcd[i][j] = rand() % 10; 
+    }  
+  }
 }
 
-void loop() {
-  //;
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  int number = millis() / 1000;
-  int digits = numDigits(number);
-  int pos = rand() % (16 - digits) - 1;
-  int row = rand() % 2 - 0;
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(pos, row);
-  // print the number of seconds since reset:
-   
-  lcd.print( number);
+int updateValue(int& value) {
+  if (value < 8) {
+    return value++;
+  } else {
+    return value = 0;
+  }
+}
+
+void updateLcdDisplay()
+{
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      updateValue(myLcd[i][j]);
+
+      lcd.setCursor(i, j);
+      lcd.print(updateValue(myLcd[i][j]));
+      delay(70);
+    }
+  }
+}
+  
+void loop() 
+{
+  updateLcdDisplay();
+  
 }
 
